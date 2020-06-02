1e92c9612be610519d0851e412277774d6adc2b9
Subhi Pandey <subhi2012cpi@gmail.com>
2020-06-02 08:19:07 +0530

    Added Search Functionality

--- MVVM/NewsApp/app/src/main/java/com/subhipandey/newsapp/repository/NewsRepository.kt
+++ MVVM/NewsApp/app/src/main/java/com/subhipandey/newsapp/repository/NewsRepository.kt
@@ -1,5 +1,6 @@
 package com.subhipandey.newsapp.repository
 
+import androidx.room.Query
 import com.subhipandey.newsapp.api.RetrofitInstance
 import com.subhipandey.newsapp.db.ArticleDatabase
 import retrofit2.Retrofit
@@ -9,4 +10,7 @@ class NewsRepository(
 ) {
     suspend fun getBreakingNews(countryCode: String, pageNumber: Int) =
         RetrofitInstance.api.getBreakingNews(countryCode, pageNumber)
+
+    suspend fun searchNews(searchQuery: String, pageNumber: Int) =
+        RetrofitInstance.api.searchForNews(searchQuery, pageNumber)
 }
--- MVVM/NewsApp/app/src/main/java/com/subhipandey/newsapp/util/Constants.kt
+++ MVVM/NewsApp/app/src/main/java/com/subhipandey/newsapp/util/Constants.kt
@@ -4,5 +4,6 @@ class Constants {
     companion object {
         const val API_KEY = "17687d720e324bfbadd9455f773b4701"
         const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 }
